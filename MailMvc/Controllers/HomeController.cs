31787424a6da06887155bf9518ab7433f7f0d45a
Friday6661 <jarifr26@gmail.com>
2023-07-09 16:53:31 +0700

    Mini Project SendEmail MailTrap with Asp.net mvc

---
+++ MailMvc/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace EmailSender.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(string recipient, string subject, string body)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Your Name", "your-email@example.com"));
+            message.To.Add(new MailboxAddress("", recipient));
+            message.Subject = subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.mailtrap.io", 587, false);
+                client.Authenticate("your-username", "your-password");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MailMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MailMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
